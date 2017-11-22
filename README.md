fd75bd19e813b9a1126cdad4aa9acc880ffcccbc
pavloko <pavlo.kochubei@hotmail.com>
2017-11-22 23:23:29 +0200

    Remove unnecessary host variable

---
+++ README.md
@@ -0,0 +1,21 @@
+# gatsby-plugin-hotjar
+
+Easily add Hotjar Analytics to your Gatsby site.
+
+## Install
+`yarn add gatsby-plugin-hotjar`
+
+## How to use
+
+```javascript
+// In your gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-plugin-hotjar`,
+    options: {
+      id: YOUR_HOTJAR_ID,
+      sv: YOUR_HOTJAR_SNIPPET_VERSION
+    },
+  },
+]
+```
--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -8,16 +8,14 @@ exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
         key={`gatsby-plugin-hotjar`}
         dangerouslySetInnerHTML={{
           __html: `
-          if (location.host === '${host}') {
-                (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:${id},hjsv:${sv}};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
-          }
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:${id},hjsv:${sv}};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
           `,
         }}
       />,
