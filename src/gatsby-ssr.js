ed290fe1c20d4aa053900860085cbe5914bfc263
pavloko <pavlo.kochubei@hotmail.com>
2017-11-22 23:04:30 +0200

    Scaffold plugin setup

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV === `production`) {
+    const {id, sv, host} = pluginOptions
+    return setPostBodyComponents([
+      <script
+        key={`gatsby-plugin-hotjar`}
+        dangerouslySetInnerHTML={{
+          __html: `
+          if (location.host === '${host}') {
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:${id},hjsv:${sv}};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+          }
+          `,
+        }}
+      />,
+    ])
+  }
+
+  return null
+}
